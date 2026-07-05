fa1ba101d9fd39b04cf99bbb849d6e9cd09335c5
ehehaye <hongxin.tang@hotmail.com>
2026-07-05 21:41:52 +0800

    chore(utils): remove unused client.js utility file
    Authored-By: trae

--- src/utils/client.js
+++
@@ -1,6 +0,0 @@
-import { AgnoClient } from '@rodrigocoliveira/agno-client';
-
-export const client = new AgnoClient({
-  endpoint: `${window.location.protocol}//${window.location.host}/agno`,
-  headers: {},
-});
